d161dea77cf096310107438b41d16b59a0ff79be
August Cook-Gregoire <augiem94@hotmail.com>
2020-03-15 13:56:54 -0400

    Forgot Probe Offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -968,7 +968,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42.5, -7.5, -2.8 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1109,7 +1109,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -2.8
-#define Y_MIN_POS -10
+#define Y_MIN_POS -9
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
